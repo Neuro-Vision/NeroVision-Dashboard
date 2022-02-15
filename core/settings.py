6e8f4b0b8c0a54197cd01ba40bcb89e7935fdb96
Harsh151200 <harshpatel151218@gmail.com>
2022-02-15 22:26:58 +0530

    wsgi setting

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
